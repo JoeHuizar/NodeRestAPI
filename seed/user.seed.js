1d3247b081fadcd68350a4a43c18cfc522ba5c94
JoeHuizar <joehuizar75@gmail.com>
2020-04-12 12:02:26 -0700

    push day one rest api

--- seed/user.seed.js
+++ seed/user.seed.js
@@ -1,8 +1,8 @@
 [{
   "id": 1,
-  "email": "lphan@shift3tech.com",
-  "username": "lphan",
-  "password": "abc123",
+  "email": "joehuizar75@gmail.com",
+  "username": "Joseph75",
+  "password": "JHuizar75",
   "super_admin": true,
   "site_admin": true,
   "hash": "CVI2C3WKYEdbL4yi3B9eE",
